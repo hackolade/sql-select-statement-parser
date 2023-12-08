cd5c0f45ac799946615ff341c12614b0b7cab2aa
Oleksandr Lytovchenko <oleksandr.lytovchenko@hackolade.com>
2023-12-08 15:13:21 +0100

    improve nested select statements

--- selectStatementListener.js
+++ selectStatementListener.js
@@ -1,9 +1,10 @@
 const SQLSelectParserListener = require('./parser/SQLSelectParserListener');
 
 class Listener extends SQLSelectParserListener {
-    constructor() {
+    constructor(parser) {
         super();
 
+        this.parser = parser;
         this.result = null;
 
         this.fieldReferences = [];
@@ -22,10 +23,29 @@ class Listener extends SQLSelectParserListener {
         return selectItems?.[0]?.name === '*';
     }
 
+    findParentJoin(ctx) {
+        const ruleIndex = this.parser.ruleNames.findIndex(ruleName => ruleName === 'joinedTable');
+
+        return this.findParent(ctx, ruleIndex);
+    }
+
+    findParent(ctx, index) {
+        if (!ctx.parentCtx) {
+            return null;
+        }
+
+        if (ctx.parentCtx.ruleIndex === index) {
+            return ctx.parentCtx;
+        } else {
+            return this.findParent(ctx.parentCtx, index);
+        }
+    }
+
     exitQuerySpecification(ctx) {
         const selectItems = ctx.selectItemList().fields || [];
+        const joinCtx = this.findParentJoin(ctx);
 
-        if (this.result && this.isAllSelected(selectItems)) {
+        if (joinCtx || this.result && this.isAllSelected(selectItems)) {
             return;
         }
 
